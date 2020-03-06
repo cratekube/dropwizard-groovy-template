b24483c90375803c110f46bf48749caa93398b80
Andrew Allen <anallen@cisco.com>
2020-03-05 21:45:42 -0500

    adding configuration for artifactory snapshot deployments
    Signed-off-by: Andrew Allen <anallen@cisco.com>

--- ci/travis_docker_build.sh
+++ ci/travis_docker_build.sh
@@ -3,21 +3,29 @@
 set -o nounset
 set -o errexit
 
+## common build args for TravisCI builds
+TRAVIS_BUILDARGS=(--build-arg TRAVIS=${TRAVIS} --build-arg TRAVIS_JOB_ID=${TRAVIS_JOB_ID})
+
 echo "[travis_docker_build] running base docker build"
-docker build --build-arg TRAVIS=${TRAVIS} --build-arg TRAVIS_JOB_ID=${TRAVIS_JOB_ID} --target Build .
+docker build ${TRAVIS_BUILDARGS[@]} --target build .
 
 # push docker images when not executing a PR build
 if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]]; then
   docker_tag="${TRAVIS_TAG:-latest}"
-  if [[ "${docker_tag}" != "latest" ]]; then
-    echo "[travis_docker_build] Uploading maven artifacts to bintray"
-    docker build --build-arg BINTRAY_USER=${BINTRAY_USER} --build-arg BINTRAY_KEY=${BINTRAY_KEY} \
-                 --build-arg BINTRAY_PUBLISH=true --build-arg APP_VERSION=${docker_tag} --target BintrayPublish .
-  fi
 
   echo "[travis_docker_build] Packaging docker images for tag ${docker_tag}"
-  docker build --build-arg TRAVIS=${TRAVIS} --build-arg TRAVIS_JOB_ID=${TRAVIS_JOB_ID} --target Package \
-               --tag ${TRAVIS_REPO_SLUG}:${docker_tag} --tag quay.io/${TRAVIS_REPO_SLUG}:${docker_tag} .
+  docker build ${TRAVIS_BUILDARGS[@]}\
+               --tag ${TRAVIS_REPO_SLUG}:${docker_tag}\
+               --tag quay.io/${TRAVIS_REPO_SLUG}:${docker_tag}\
+               --target package .
+
+  ## setup different build args if the build is for a snapshot or a tag
+  PUBLISH_OPTS=(--build-arg TRAVIS=${TRAVIS} --build-arg TRAVIS_JOB_ID=${TRAVIS_JOB_ID} --build-arg BUILDKIT_INLINE_CACHE=1 --build-arg JFROG_DEPLOY_USER=${JFROG_DEPLOY_USER} --build-arg JFROG_DEPLOY_KEY=${JFROG_DEPLOY_KEY})
+  if [[ ! -z "${TRAVIS_TAG+x}" ]]; then
+    PUBLISH_OPTS+=(--build-arg BINTRAY_PUBLISH=true --build-arg APP_VERSION=${TRAVIS_TAG})
+  fi
+  echo "[travis_docker_build] Uploading maven artifacts"
+  docker build ${TRAVIS_BUILDARGS[@]} ${PUBLISH_OPTS[@]} --target publish .
 
   echo "[travis_docker_build] dockerhub push for tag ${docker_tag}"
   docker login -u="${DOCKERHUB_USER}" -p="${DOCKERHUB_PASS}"
@@ -27,5 +35,5 @@ if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]]; then
   docker login -u="${QUAYIO_USER}" -p="${QUAYIO_PASS}" quay.io
   docker push quay.io/${TRAVIS_REPO_SLUG}:${docker_tag}
 else
-  echo "[travis_docker_build] Skipping Docker publish for PR build"
+  echo "[travis_docker_build] Skipping Docker and Maven publish for PR build"
 fi
